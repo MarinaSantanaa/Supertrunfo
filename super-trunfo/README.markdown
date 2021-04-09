c6e5ca05fd818ce10a604441adf8e74eb293e79d
MarinaSantanaa <79662195+MarinaSantanaa@users.noreply.github.com>
2021-04-09 20:45:33 -0300

    Add files via upload

---
+++ super-trunfo/README.markdown
@@ -0,0 +1,5 @@
+# Super Trunfo 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/MarinaSantanaa/pen/eYgvgqW](https://codepen.io/MarinaSantanaa/pen/eYgvgqW).
+
+
---
+++ super-trunfo/src/script.js
@@ -0,0 +1,111 @@
+var cartaMaria = {
+    nome: "Vanellope",
+    imagem: "https://static.wikia.nocookie.net/detonaralph/images/b/be/Vanellope.jpg/revision/latest/top-crop/width/360/height/450?cb=20130307205955&path-prefix=pt-br",
+    atributos: {
+        Fofura: 20,
+        Conversa: 80,
+        Medo: 10
+    }
+}
+
+var cartaMarina = {
+    nome: "Boo",
+    imagem:"https://img.elo7.com.br/product/zoom/18F05E1/touca-boo-mostros-sa-monstros-sa.jpg",
+    atributos: {
+        Fofura: 80,
+        Conversa: 10,
+        Medo: 60
+    }
+}
+
+var cartaMariana = {
+    nome: "Agnes",
+    imagem: "https://i.pinimg.com/originals/7c/f4/d9/7cf4d9d1c1ae3b16e4efc7a4e856f2fc.jpg",
+    atributos: {
+        Fofura: 75,
+        Conversa: 20,
+        Medo: 40
+    }
+}
+
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaMaria, cartaMarina, cartaMariana]
+// 0          1           2
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 3)
+    cartaMaquina = cartas[numeroCartaMaquina]
+
+    var numeroCartaJogador = parseInt(Math.random() * 3)
+    while (numeroCartaJogador == numeroCartaMaquina) {
+        numeroCartaJogador = parseInt(Math.random() * 3)
+    }
+    cartaJogador = cartas[numeroCartaJogador]
+    console.log(cartaJogador)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibeCartaJogador()
+}
+
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+
+    divResultado.innerHTML = htmlResultado
+    exibeCartaMaquina()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        console.log(atributo)
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
